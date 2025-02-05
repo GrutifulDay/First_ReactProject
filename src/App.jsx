ef8f4982ad329bce801eb3705db7bc2b4ec7f1f6
Tereza <tereza.zizkova366@gmail.com>
2025-02-05 14:31:31 +0100

    nastaveni click

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import componentsImg from './assets/components.png'
 import { CORE_CONCEPTS } from './data.js'
 import Header from './components/Header/Header.jsx'
 import CoreConcept from './components/CoreConcept.jsx'
+import TabButton from './components/TabButton.jsx'
 
 
 
 
 function App() {
+  function handleSelect() {
+    console.log("hello world-select");
+}
+
   return (
     <div>
        <Header />
@@ -24,7 +29,16 @@ function App() {
           <CoreConcept {...CORE_CONCEPTS[3]}/>
           </ul>
         </section>
-        <h2>Time to get started!</h2>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+          <TabButton onSelect={handleSelect}>Components</TabButton>
+          <TabButton>JSX</TabButton>
+          <TabButton>Props</TabButton>
+          <TabButton>State</TabButton>
+            
+          </menu>
+        </section>
       </main>
     </div>
   );
