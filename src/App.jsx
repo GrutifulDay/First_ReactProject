27877902b786a341eb9cd137f82061b3b4875981
Tereza <tereza.zizkova366@gmail.com>
2025-02-10 10:07:03 +0100

    vytvoreni komponenty s children jako nasledny prvek pro zobrazeny vystup

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,7 @@ import TabButton from './components/TabButton.jsx'
 
 
 function App() {
-  useState()
 
-  function handleSelect(selectedButton) {
-    let tabContent = "Please click on button"
-
-    tabContent = selectedButton
-
-    console.log(tabContent);
-}
 
   return (
     <div>
@@ -40,10 +32,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-          <TabButton onSelect={() => handleSelect ('components')}>Components</TabButton>
-          <TabButton onSelect={() => handleSelect ('jsx')}>JSX</TabButton>
-          <TabButton onSelect={() => handleSelect ('props')}>Props</TabButton>
-          <TabButton onSelect={() => handleSelect ('state')}>State</TabButton>
+          <TabButton>Components</TabButton>
+          <TabButton>JSX</TabButton>
+          <TabButton>Props</TabButton>
+          <TabButton>State</TabButton>
             
           </menu>
           
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,7 +1,7 @@
-export default function TabButton({children, onSelect}) {
+export default function TabButton({children}) {
     return (
         <li>
-            <button onClick={onSelect}>{children}</button>
+            <button>{children}</button>
         </li>
     )
     
